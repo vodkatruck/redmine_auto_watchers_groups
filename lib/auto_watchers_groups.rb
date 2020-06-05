cd2fe463c812810e7d134356a201ab4c77c0fadb
vodkatruck <63997465+vodkatruck@users.noreply.github.com>
2020-06-05 14:34:35 +0400

    Update auto_watchers_groups.rb

--- lib/auto_watchers_groups.rb
+++ lib/auto_watchers_groups.rb
@@ -1,4 +1,4 @@
-module AutoWatchersFromGroups
+module AutoWatchersGroups
         class Hooks < Redmine::Hook::ViewListener
                 def controller_issues_new_after_save(context)
                         auto_watchers(context)
@@ -7,19 +7,27 @@ module AutoWatchersFromGroups
                         auto_watchers(context)
                 end
                 def auto_watchers(context)
-                        @settings ||= Setting.plugin_redmine_auto_watchers_from_groups
-                        @issue = Issue.find context[:issue][:id]
-                        grom = 1
-                        for grom in 1..100
-                                group_id = "#{grom}"
-                                if @settings['groups_enabled'].include? group_id
-                                        group = Group.find group_id
-                                        group.users.each do |new_watcher|
-                                                Watcher.create(:watchable => @issue, :user => new_watcher)
-                                        end
-                                end
-                        end
+					@settings ||= Setting.plugin_redmine_auto_watchers_groups
+                    @issue = Issue.find context[:issue][:id]
+					Role.all.sort.each do |role|
+						Group.all.sort.each do |group|
+							group.users.each do |user|
+								if @settings['groups_enabled'].include? "#{group.id}"
+									if @settings['roles_enabled'].include? "#{role.id}"
+										if user.roles.ids.include? role.id
+											Watcher.create(:watchable => @issue, :user => user)
+										end
+									end
+								end
+							end
+						end
+					end
                 end
                 alias_method :controller_issues_bulk_edit_before_save, :controller_issues_edit_before_save
-        end 
+        end
+		
 end
+
+
+
+
