44d585305ce773eee5303bbdfb69910686eb7998
vodkatruck <63997465+vodkatruck@users.noreply.github.com>
2020-06-08 09:56:07 +0400

    Release

--- lib/auto_watchers_groups.rb
+++ lib/auto_watchers_groups.rb
@@ -7,27 +7,28 @@ module AutoWatchersGroups
                         auto_watchers(context)
                 end
                 def auto_watchers(context)
-					@settings ||= Setting.plugin_redmine_auto_watchers_groups
-                    @issue = Issue.find context[:issue][:id]
-					Role.all.sort.each do |role|
-						Group.all.sort.each do |group|
-							group.users.each do |user|
-								if @settings['groups_enabled'].include? "#{group.id}"
-									if @settings['roles_enabled'].include? "#{role.id}"
-										if user.roles.ids.include? role.id
-											Watcher.create(:watchable => @issue, :user => user)
-										end
-									end
-								end
-							end
-						end
-					end
+                        @settings ||= Setting.plugin_redmine_auto_watchers_groups
+                        @issue = Issue.find context[:issue][:id]
+                        Role.all.sort.each do |role|
+                                if @settings['roles_enabled'].include? "#{role.id}"
+                                        role.members.each do |member|
+                                                if member.project_id = @issue.project_id
+                                                        Group.all.sort.each do |group|
+                                                                if @settings['groups_enabled'].include? "#{group.id}"
+                                                                        group.users.each do |user|
+                                                                                if member.user_id = user.id
+                                                                                        Watcher.create(:watchable => @issue, :user => user)
+                                                                                end
+                                                                        end
+                                                                end
+                                                        end
+                                                end
+                                        end
+                                end
+                        end
                 end
                 alias_method :controller_issues_bulk_edit_before_save, :controller_issues_edit_before_save
         end
-		
+
 end
-
-
-
 
