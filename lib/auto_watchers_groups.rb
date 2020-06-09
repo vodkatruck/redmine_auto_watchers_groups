61e72b7fcc982c49f374f8ce71d6a67067c2e826
vodkatruck <63997465+vodkatruck@users.noreply.github.com>
2020-06-09 10:56:27 +0400

    Fix of crush if group/role/role_and_groud not chou

--- lib/auto_watchers_groups.rb
+++ lib/auto_watchers_groups.rb
@@ -9,26 +9,44 @@ module AutoWatchersGroups
                 def auto_watchers(context)
                         @settings ||= Setting.plugin_redmine_auto_watchers_groups
                         @issue = Issue.find context[:issue][:id]
-                        Role.all.sort.each do |role|
-                                if @settings['roles_enabled'].include? "#{role.id}"
-                                        role.members.each do |member|
-                                                if member.project_id == @issue.project_id
-                                                        Group.all.sort.each do |group|
-                                                                if @settings['groups_enabled'].include? "#{group.id}"
-                                                                        group.users.each do |user|
-                                                                                if member.user_id == user.id
-                                                                                        Watcher.create(:watchable => @issue, :user => user)
-                                                                                end
-                                                                        end
-                                                                end
+                        unless @settings['roles_enabled'].nil?
+                            Role.all.sort.each do |role|
+                                    if @settings['roles_enabled'].include? "#{role.id}"
+                                            role.members.each do |member|
+                                                    if member.project_id == @issue.project_id
+                                                        unless @settings['groups_enabled'].nil?
+                                                            Group.all.sort.each do |group|
+                                                                    if @settings['groups_enabled'].include? "#{group.id}"
+                                                                            group.users.each do |user|
+                                                                                    if member.user_id == user.id
+                                                                                            Watcher.create(:watchable => @issue, :user => user)
+                                                                                    end
+                                                                            end
+                                                                    end
+                                                            end
+                                                        else
+                                                                                            user = User.find member.user_id
+                                                                                            Watcher.create(:watchable => @issue, :user => user)
+                                                                                    
                                                         end
-                                                end
-                                        end
-                                end
+                                                    end
+                                            end
+                                    end
+                            end 
+                        else
+                            unless @settings['groups_enabled'].nil?
+                                                                    if @settings['groups_enabled'].include? "#{group.id}"
+                                                                            group.users.each do |user|
+                                                                                            Watcher.create(:watchable => @issue, :user => user)
+                                                                            end
+                                                                    end
+                            end
                         end
+                        
                 end
                 alias_method :controller_issues_bulk_edit_before_save, :controller_issues_edit_before_save
         end
 
 end
+
 
